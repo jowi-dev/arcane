06895ab7ad7c69942943c67d710b4bab89207461
jowi-dev <joey8williams@gmail.com>
2024-11-22 12:30:47 -0500

    more cleanup

--- lib/arcane.ex
+++ lib/arcane.ex
@@ -5,25 +5,22 @@ defmodule Arcane do
   This module serves as an entry point for interacting with all points of the compiler
   """
   alias Arcane.Compiler
-  alias Arcane.Lexer
   alias Arcane.Parser
 
   @doc "Compile the expression and dump LLVM IR to stdout"
   def compile_expression(expr) do
     expr
-    |> Lexer.pass_through()
     |> Parser.pass_through()
     |> Compiler.compile_s_expression()
     |> Compiler.compile_llvm(:stdout)
   end
 
   @doc "Compile the expression and create a runnable executable"
-  def compile_and_build(expr, output_name \\ "program") do
+  def compile_and_build(expr, output_name \\ "arcane") do
     # Ensure application is started
     Application.ensure_all_started(:arcane)
 
     expr
-    |> Lexer.pass_through()
     |> Parser.pass_through()
     |> Compiler.compile_s_expression()
     |> Compiler.compile_llvm(output_name)
